0edb9556fb34a117f2be3f5ad051b589a75a9cfa
mfalconaro <matias.falconaro@datco.net>
2023-12-14 15:00:50 -0300

    Implementar persistencia de datos - Funcionalidad consulta - Uso de Regex
    Simplificación de Regex
    
    Reemplazar el uso de 'isinstance'
    
    Fix identation - PEP8

--- app/proyeto_final.py
+++ app/proyeto_final.py
@@ -1,3 +1,5 @@
+import re
+import sqlite3
 import tkinter as tk
 
 from tkinter import *
@@ -11,11 +13,9 @@ from PIL import Image, ImageTk
 total_acumulado=0
 mensaje_prompt = "Bienvenido."
 
-
 #-----------------------------INICIO CONTROLADOR-----------------------------#
 
 def actualizar_estado_bar():
-    
     global mensaje_prompt
     
     estado.config(text=mensaje_prompt)  # Actualiza texto del label
@@ -23,7 +23,6 @@ def actualizar_estado_bar():
 
 
 def actualizar_estado_fecha():
-    
     if var_check_vencimiento.get():
         e_vencimiento.config(state='disabled')
     else:
@@ -31,22 +30,26 @@ def actualizar_estado_fecha():
 
 
 def validar_campos():
-
-    for var in [var_producto, var_cantidad,
-                var_monto, var_proveedor,
-                var_fecha, var_vencimiento]:
-        if isinstance(var, (StringVar, IntVar)) and not var.get():
+    campos_var = [var_producto,
+                  var_cantidad,
+                  var_monto,
+                  var_proveedor,
+                  var_fecha,
+                  var_vencimiento]
+    campos_cb = [cb_responsable, cb_rubro, cb_medio_pago]
+    
+    for var in campos_var:
+        if (type(var) == str or type(var) == int) and not var:
             return False
 
-    for cb in [cb_responsable, cb_rubro, cb_medio_pago]:
-        if not cb.get():
+    for cb in campos_cb:
+        if not cb:
             return False
 
     return True
 
 
 def limpiar_formulario():
-    
     var_monto.set('')
     var_producto.set('')
     var_cantidad.set('')
@@ -59,54 +62,53 @@ def limpiar_formulario():
     cb_medio_pago.set('')
     
     
-def aplicar_modificacion(compra_id):
-    
+def aplicar_modificacion(compra_id, id_bd):
     global mensaje_prompt
     
-    responsable_actual = cb_responsable.get()
-    rubro_actual = cb_rubro.get()
-    medio_pago_actual = cb_medio_pago.get()
-
-    vencimiento_value = 'N/A' if var_check_vencimiento.get() else e_vencimiento.get_date().strftime("%Y-%m-%d")
+    if not validar_campos():
+        mensaje_prompt = "Todos los campos deben estar llenos."
+        actualizar_estado_bar()
+        showinfo("Info", mensaje_prompt)
+        return
 
     nuevo_valor = {
         'producto': var_producto.get(),
         'cantidad': int(var_cantidad.get()),
         'monto': float(var_monto.get()),
-        'responsable': responsable_actual,
-        'rubro': rubro_actual,
+        'responsable': cb_responsable.get(),
+        'rubro': cb_rubro.get(),
         'proveedor': var_proveedor.get(),
-        'medio_pago': medio_pago_actual,
+        'medio_pago': cb_medio_pago.get(),
         'fecha': var_fecha.get(),
-        'vencimiento': vencimiento_value
+        'vencimiento': var_vencimiento.get()
     }
-    
-    nuevo_subtotal = nuevo_valor['monto'] * nuevo_valor['cantidad']
-    var_total.set(f"{nuevo_subtotal:.2f}")
-    
+
+    modificacion_bd(id_bd, nuevo_valor)
+
     if tree.exists(compra_id):
         tree.item(compra_id, values=(
             nuevo_valor['producto'],
             nuevo_valor['cantidad'],
             nuevo_valor['monto'],
             nuevo_valor['responsable'],
-            f"{nuevo_subtotal:.2f}",
+            nuevo_valor['monto'] * nuevo_valor['cantidad'], # Subtotal
             nuevo_valor['rubro'],
             nuevo_valor['proveedor'],
             nuevo_valor['medio_pago'],
             nuevo_valor['fecha'],
             nuevo_valor['vencimiento']
         ))
-    
-    boton_alta.config(text='ALTA', command=alta)
-    
-    mensaje_prompt = "Modificación del registro con ID: " + str(compra_id)
+
+    mensaje_prompt = "Registro modificado con ID: " + str(id_bd)
     actualizar_estado_bar()
     limpiar_formulario()
+    boton_alta.config(text='ALTA', command=alta)
 
-#-----BASE DE DATOS-----#
 
-#-----FIN BASE DE DATOS-----#
+def cargar_datos_en_treeview():
+    registros = consulta_bd()
+    for row in registros:
+        tree.insert('', 'end', text=str(row[0]), values=row[1:])
 
 #-------------------------------FIN CONTROLADOR------------------------------#
 
@@ -116,15 +118,12 @@ def aplicar_modificacion(compra_id):
 
 #-----ABMC-----#
 def alta():
-    
     global total_acumulado
     global mensaje_prompt
-
+    
     var_fecha.set(cal_fecha.get_date().strftime("%Y-%m-%d"))
-
     if var_check_vencimiento.get():
         vencimiento_value = 'N/A'
-        e_vencimiento.config(state='disabled')
     else:
         vencimiento_value = e_vencimiento.get_date().strftime("%Y-%m-%d")
 
@@ -154,35 +153,34 @@ def alta():
             showinfo("Info", mensaje_prompt)
             actualizar_estado_bar()
             return
+        
+    conn = conectar_base_de_datos()
+    ultimo_id = alta_bd(conn, valores)
 
     subtotal_acumulado = valores['cantidad'] * valores['monto']
     total_acumulado += subtotal_acumulado
-
     var_total.set(f"{total_acumulado:.2f}")
 
-    compra_id = tree.insert('',
-                            'end',
-                            values=(valores['producto'],
-                                    valores['cantidad'],
-                                    valores['monto'],
-                                    valores['responsable'],
-                                    f"{subtotal_acumulado:.2f}",
-                                    valores['rubro'],
-                                    valores['proveedor'],
-                                    valores['medio_pago'],
-                                    valores['fecha'],
-                                    valores['vencimiento']))
+    tree.insert('',
+                'end',
+                text=str(ultimo_id),
+                values=(valores['producto'],
+                        valores['cantidad'],
+                        valores['monto'],
+                        valores['responsable'],
+                        f"{subtotal_acumulado:.2f}",
+                        valores['rubro'],
+                        valores['proveedor'],
+                        valores['medio_pago'],
+                        valores['fecha'],
+                        valores['vencimiento']))
 
-    tree.item(compra_id, text=str(compra_id))
-
-    var_id.set(compra_id)
-    mensaje_prompt = "Se dio de alta el registro con ID: " + str(compra_id)
+    mensaje_prompt = "Se dio de alta el registro con ID: " + str(ultimo_id)
     actualizar_estado_bar()
     limpiar_formulario()
 
 
 def baja():
-    
     global total_acumulado
     global mensaje_prompt
     
@@ -194,30 +192,42 @@ def baja():
         return
     
     valores = tree.item(compra_id, 'values')
-    
-    try:
-        subtotal_eliminar = float(valores[4])  
-    except ValueError:
-        mensaje_prompt = "Subtotal para el registro seleccionado."
+
+    subtotal_str = valores[4]
+
+    if re.match(r'^\d+(\.\d+)?$', subtotal_str): # Enteros => 0 y decimales > 0
+        subtotal_eliminar = float(subtotal_str)
+    else:
+        mensaje_prompt = "El subtotal no es un número válido."
         showinfo("Error", mensaje_prompt)
         actualizar_estado_bar()
         return
-    
+
+    id_bd_str = tree.item(compra_id, 'text')
+
+    if re.match(r'^\d+$', id_bd_str): # Enteros => 0
+        id_bd = int(id_bd_str)
+    else:
+        mensaje_prompt = "El ID no es un número válido."
+        showinfo("Error", mensaje_prompt)
+        actualizar_estado_bar()
+        return
+
+    baja_bd(id_bd)
+
     total_acumulado -= subtotal_eliminar
     var_total.set(f"{total_acumulado:.2f}")
     
     tree.delete(compra_id)
     
-    mensaje_prompt = "Se dio de baja el registro con ID: " + str(compra_id)
+    mensaje_prompt = "Se dio de baja el registro con ID: " + str(id_bd)
     actualizar_estado_bar()
     
     
 def modificacion():
-    
     global mensaje_prompt
     
     compra_id = tree.focus()
-    
     if not compra_id:
         mensaje_prompt = "Debe seleccionar un registro para Modificar."
         showinfo("Info", mensaje_prompt)
@@ -225,28 +235,167 @@ def modificacion():
         return
     
     valores = tree.item(compra_id, 'values')
+    id_bd = int(tree.item(compra_id, 'text'))
     
     var_producto.set(valores[0])
     var_cantidad.set(valores[1])
     var_monto.set(valores[2])
     var_fecha.set(valores[8])
-    
     cb_responsable.set(valores[3])
     cb_rubro.set(valores[5])
     cb_medio_pago.set(valores[7])
-    
     var_proveedor.set(valores[6])
     var_vencimiento.set(valores[9])
     
     boton_alta.config(text='MODIFICAR',
-                      command=lambda: aplicar_modificacion(compra_id))
-    mensaje_prompt = "Mostrando datos para modificar..."
+                      command=lambda: aplicar_modificacion(compra_id, id_bd))
+    mensaje_prompt = "Modificando registro ID: " + str(id_bd)
     actualizar_estado_bar()
 
 
 def consulta():
-    pass
+    global mensaje_prompt
+
+    termino_busqueda = var_consulta.get()
+    registros = consulta_bd()
+
+    # Crea objeto regex desde 'termino_busqueda' NO sensible a mayúsculas
+    regex = re.compile(termino_busqueda, re.IGNORECASE) 
+
+    registros_filtrados = []
+    for row in registros:
+        row_str = ' '.join(map(str, row))  # Registro -> cadena
+        if regex.search(row_str):
+            registros_filtrados.append(row)
+
+    for i in tree.get_children():
+        tree.delete(i)
+
+    for row in registros_filtrados:
+        tree.insert('', 'end', text=str(row[0]), values=row[1:])
+
+    mensaje_prompt = f"Resultados de la búsqueda para: {termino_busqueda}"
+    actualizar_estado_bar()
 #-----FIN ABMC-----#
+
+#-----BASE DE DATOS-----#
+def conectar_base_de_datos():
+    conn = sqlite3.connect('BD/base_de_datos.db')
+    return conn
+
+
+def desconectar_base_de_datos(conn):
+    conn.close()
+
+
+def crear_tabla(conn):
+    cursor = conn.cursor()
+    query = """CREATE TABLE IF NOT EXISTS gastos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            producto_servicio TEXT,
+            cantidad INTEGER,
+            monto FLOAT,
+            responsable TEXT,
+            subtotal FLOAT,
+            rubro text,
+            proveedor TEXT,
+            medio_de_pago TEXT,
+            fecha DATE,
+            vencimiento DATE
+            );"""
+    cursor.execute(query)
+    conn.commit()
+
+
+def alta_bd(conn, valores):
+    conn = conectar_base_de_datos()
+    cursor = conn.cursor()
+
+    query = """INSERT INTO gastos (producto_servicio,
+            cantidad,
+            monto,
+            responsable,
+            subtotal,
+            rubro, proveedor,
+            medio_de_pago,
+            fecha,
+            vencimiento)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
+               
+    subtotal = valores['cantidad'] * valores['monto']
+    
+    data = (valores['producto'], 
+            valores['cantidad'], 
+            valores['monto'], 
+            valores['responsable'], 
+            subtotal, valores['rubro'], 
+            valores['proveedor'], 
+            valores['medio_pago'], 
+            valores['fecha'], 
+            valores['vencimiento'])
+
+    cursor.execute(query, data)
+    conn.commit()
+    ultimo_id = cursor.lastrowid 
+    desconectar_base_de_datos(conn)
+    return ultimo_id
+    
+
+def baja_bd(id_registro):
+    conn = conectar_base_de_datos()
+    cursor = conn.cursor()
+    query = "DELETE FROM gastos WHERE id = ?;"
+    cursor.execute(query, (id_registro,))
+    conn.commit()
+    desconectar_base_de_datos(conn)
+
+
+def modificacion_bd(id_registro, valores):
+    conn = conectar_base_de_datos()
+    cursor = conn.cursor()
+    query = """UPDATE gastos SET
+            producto_servicio = ?,
+            cantidad = ?,
+            monto = ?,
+            responsable = ?,
+            subtotal = ?,
+            rubro = ?,
+            proveedor = ?,
+            medio_de_pago = ?,
+            fecha = ?,
+            vencimiento = ?
+            WHERE id = ?;"""
+
+    subtotal = valores['cantidad'] * valores['monto']
+    data = (valores['producto'],
+            valores['cantidad'],
+            valores['monto'],
+            valores['responsable'],
+            subtotal, valores['rubro'],
+            valores['proveedor'],
+            valores['medio_pago'],
+            valores['fecha'],
+            valores['vencimiento'],
+            id_registro)
+
+    cursor.execute(query, data)
+    conn.commit()
+    desconectar_base_de_datos(conn)
+
+    
+def consulta_bd():
+    conn = conectar_base_de_datos()
+    cursor = conn.cursor()
+    query = """SELECT * FROM gastos;"""
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    desconectar_base_de_datos(conn)
+    return rows
+
+
+conn = conectar_base_de_datos()
+crear_tabla(conn)
+#-----FIN BASE DE DATOS-----#
 
 #---------------------------------FIN MODELO---------------------------------#
 
@@ -294,8 +443,13 @@ var_check_vencimiento = BooleanVar()
 var_consulta = StringVar()
 
 #-----VALIDACIONES-----#
-variables_a_validar = [var_producto, var_cantidad, var_monto, var_responsable,
-                       var_proveedor, var_medio_pago, var_rubro, var_fecha, 
+variables_a_validar = [var_producto,
+                       var_cantidad,
+                       var_monto,
+                       var_responsable,
+                       var_proveedor,
+                       var_medio_pago,
+                       var_rubro, var_fecha, 
                        var_vencimiento]
 
 opciones_rubro = ["Mantenimiento", "Impuestos", "Servicios",
@@ -459,5 +613,6 @@ tree.heading('col10', text='Vencimiento')
 
 #-----FIN WIDGETS-----#
 
+cargar_datos_en_treeview()
 root.mainloop()
 #---------------------------------FIN VISTA----------------------------------#
